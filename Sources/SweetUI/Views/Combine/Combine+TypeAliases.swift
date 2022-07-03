f68b97297d29bb00e811228e15f4dacf11a17d27
Benedict Cohen <ben@benedictcohen.co.uk>
2022-07-03 15:49:54 +0100

    Removes redunant extension

--- Sources/SweetUI/Views/Combine/Combine+TypeAliases.swift
+++ Sources/SweetUI/Views/Combine/Combine+TypeAliases.swift
@@ -7,21 +7,3 @@ public typealias ViewState<T> = AnyPublisher<T, Never>
 #else
 public typealias ViewState<T> = any Publisher<T, Never>
 #endif
-
-
-// MARK: - Helper
-
-public extension CurrentValueSubject {
-
-    func send(using transform: (inout Output) -> Void) {
-        var value = self.value
-        transform(&value)
-        self.send(value)
-    }
-
-    func send(using transform: (Output) -> Output) {
-        let stale = self.value
-        let fresh = transform(stale)
-        self.send(fresh)
-    }
-}
