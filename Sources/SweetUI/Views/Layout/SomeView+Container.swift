9c46438c5b305e1bf7ab21223916eb7fba932d73
Benedict Cohen <ben@benedictcohen.co.uk>
2022-08-02 13:45:02 +0100

    Adds unarranged content container

---
+++ Sources/SweetUI/Views/Layout/SomeView+Container.swift
@@ -0,0 +1,15 @@
+import Foundation
+import UIKit
+
+
+public extension SomeView {
+
+    func container(withLayoutConfiguration configure: (Container<Self>, Self) -> Void) -> Container<Self> {
+        let container = Container(content: self)
+        container.addSubview(self)
+        self.translatesAutoresizingMaskIntoConstraints = false
+
+        configure(container, self)
+        return container
+    }
+}
