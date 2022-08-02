284b94e95a0b638e6a044184a6a0a9a60d0f3899
Benedict Cohen <ben@benedictcohen.co.uk>
2022-08-02 13:41:41 +0100

    Adds TraitCollectionDidChangeProvider to onTraitCollectionChange handler

--- Sources/SweetUI/Views/Layout/SomeView+TraitCollectionDidChange.swift
+++ Sources/SweetUI/Views/Layout/SomeView+TraitCollectionDidChange.swift
@@ -3,12 +3,15 @@ import UIKit
 
 public extension SomeView {
 
-    func onTraitCollectionChange<T: TraitCollectionDidChangeProvider>(handlerIdentifier: AnyHashable = UUID(), of provider: T, _ handler: @escaping (Self, UITraitCollection?, UITraitCollection) -> Void) -> Self {
-        provider.addTraitCollectionDidChangeHandler(withIdentifier: handlerIdentifier) { [weak self] previous, current in
-            guard let self = self else {
+    func onTraitCollectionChange<T: TraitCollectionDidChangeProvider>(handlerIdentifier: AnyHashable = UUID(), of provider: T, _ handler: @escaping (Self, T, UITraitCollection?, UITraitCollection) -> Void) -> Self {
+        provider.addTraitCollectionDidChangeHandler(withIdentifier: handlerIdentifier) { [weak self, weak provider] previous, current in
+            guard
+                let self = self,
+                let provider = provider
+            else {
                 return
             }
-            handler(self, previous, current)
+            handler(self, provider, previous, current)
         }
         return self
     }
