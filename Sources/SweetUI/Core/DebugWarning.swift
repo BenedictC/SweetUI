027d09c9a0574ba9cb4d4b7e014cb011f6aa9091
Benedict Cohen <ben@benedictcohen.co.uk>
2022-09-19 14:50:36 +0100

    Updates debug warning to not throw by default

--- Sources/SweetUI/Core/DebugWarning.swift
+++ Sources/SweetUI/Core/DebugWarning.swift
@@ -3,12 +3,14 @@ struct DebugWarning: Error, CustomDebugStringConvertible {
     let message: String
     var debugDescription: String { message }
 
-    static func raise(_ message: String) {
-        do {
-            // We throw so the 'Swift Error' breakpoint kicks in and prevents the warning from being lost in the console
-            throw DebugWarning(message: message)
-        } catch {
-            print("⚠️ Debug warning: \(error)")
+    static func raise(_ message: String, shouldThrow: Bool = false) {
+        if shouldThrow {
+            do {
+                throw DebugWarning(message: message)
+            } catch {
+                // We throw so the 'Swift Error' breakpoint kicks in and prevents the warning from being lost in the console
+            }
         }
+        print("⚠️ Debug warning: \(message)")
     }
 }
