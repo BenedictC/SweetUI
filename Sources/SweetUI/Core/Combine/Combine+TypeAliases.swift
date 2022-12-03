bd1d9555532a64af3a76b0bde2fba98a26366ad1
Benedict Cohen <ben@benedictcohen.co.uk>
2022-12-03 10:42:14 +0000

    Adds State & Binding property wrappers

--- Sources/SweetUI/Core/Combine/Combine+TypeAliases.swift
+++ Sources/SweetUI/Core/Combine/Combine+TypeAliases.swift
@@ -3,3 +3,41 @@ import Combine
 
 public typealias ViewBinding<T> = AnySubject<T, Never>
 public typealias ViewState<T> = AnyPublisher<T, Never>
+
+
+// MARK: -
+
+@propertyWrapper
+public struct State<T> {
+
+    public var wrappedValue: T {
+        get { subject.value }
+        set { subject.send(newValue) }
+    }
+
+    public let projectedValue: AnyPublisher<T, Never>
+    private let subject: CurrentValueSubject<T, Never>
+
+    public init(wrappedValue: T) {
+        subject = CurrentValueSubject(wrappedValue)
+        projectedValue = subject.eraseToAnyPublisher()
+    }
+}
+
+
+@propertyWrapper
+public struct Binding<T> {
+
+    public var wrappedValue: T {
+        get { subject.value }
+        set { subject.send(newValue) }
+    }
+
+    public let projectedValue: AnySubject<T, Never>
+    private let subject: CurrentValueSubject<T, Never>
+
+    public init(wrappedValue: T) {
+        subject = CurrentValueSubject(wrappedValue)
+        projectedValue = subject.eraseToAnySubject()
+    }
+}
