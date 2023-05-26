7b3a6821e04588d58e8c480b3a0172fb54d9bb7d
Benedict Cohen <ben@benedictcohen.co.uk>
2023-05-26 18:01:34 +0100

    Adds UIAlertController subclass suitable for async/await presentation

---
+++ Sources/SweetUI/Controllers/Modal/AlertController.swift
@@ -0,0 +1,168 @@
+import Combine
+import UIKit
+
+
+// MARK: - AlertController
+
+open class AlertController<T>: UIAlertController, Presentable {
+
+    // MARK: Types
+
+    public typealias Success = T
+
+    public enum Component {
+        case text(AlertTextSubject<String?, Never>, configuration: (UITextField) -> Void = { _ in })
+        case attributedText(AlertTextSubject<NSAttributedString?, Never>, configuration: (UITextField) -> Void = { _ in })
+        case action(title: String, style: UIAlertAction.Style, result: Result<T, Error>, isPreferred: Bool)
+
+        public static func cancelAction(_ title: String, error: Error = PresentableError.cancelled) -> Self {
+            .action(title: title, style: .cancel, result: .failure(error), isPreferred: false)
+        }
+
+        public static func cancelAction(_ title: String, value: T) -> Self {
+            .action(title: title, style: .cancel, result: .success(value), isPreferred: false)
+        }
+
+        public static func destructiveAction(_ title: String, value: T) -> Self {
+            .action(title: title, style: .destructive, result: .success(value), isPreferred: false)
+        }
+
+        public static func standardAction(_ title: String, value: T) -> Self {
+            .action(title: title, style: .default, result: .success(value), isPreferred: false)
+        }
+
+        public static func preferred(_ action: Self) -> Self {
+            switch action {
+            case .text, .attributedText:
+                print("Attempted to erroneously set a textField as a preferred action.")
+                return action
+                
+            case .action(let title, let style, let result, _):
+                return .action(title: title, style: style, result: result, isPreferred: true)
+            }
+        }
+    }
+
+
+    // MARK: Properties
+
+    private var continuation: CheckedContinuation<Success, Error>?
+
+
+    // MARK: Instance life cycle
+
+    public convenience init(title: String?, message: String? = nil, preferredStyle: UIAlertController.Style = .alert, components: [Component]) {
+        self.init(title: title, message: message, preferredStyle: preferredStyle)
+
+        var preferredAction: UIAlertAction?
+        for component in components {
+            switch component {
+            case .action(let title, let style, let result, let isPreferred):
+                let action = UIAlertAction(title: title, style: style) { [weak self] _ in
+                    self?.continuation?.resume(with: result)
+                }
+                addAction(action)
+                if isPreferred {
+                    preferredAction = action
+                }
+
+            case .attributedText(let subject, let configuration):
+                addTextField { textField in
+                    textField.placeholder = subject.placeholder
+                    configuration(textField)
+                    _ = textField.bindAttributedText(to: subject)
+                }
+
+            case .text(let subject, let configuration):
+                addTextField { textField in
+                    textField.placeholder = subject.placeholder
+                    configuration(textField)
+                    _ = textField.bindText(to: subject)
+                }
+            }
+        }
+        self.preferredAction = preferredAction
+    }
+
+
+    // MARK: View life cycle
+
+    open override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        didDisappear()
+    }
+
+
+    // MARK: Presentable
+
+    public func fulfilContinuationForCancelledPresentation(_ continuation: CheckedContinuation<Success, Error>) {
+        self.continuation = continuation
+    }
+}
+
+
+// MARK: - Supporting types
+
+@propertyWrapper
+public struct AlertText {
+
+    public var wrappedValue: String? {
+        get { projectedValue.value }
+        // nonmutating is to avoid crashes due to re-entrant when the get is called by a subscriber of the underlying subject
+        nonmutating
+        set { projectedValue.send(newValue) }
+    }
+    public let projectedValue: AlertTextSubject<String?, Never>
+
+    public init(wrappedValue: String?, placeholder: String? = nil) {
+        self.projectedValue = AlertTextSubject(placeholder: placeholder, initialValue: wrappedValue)
+    }
+}
+
+
+@propertyWrapper
+public struct AlertAttributedText {
+
+    public var wrappedValue: NSAttributedString? {
+        get { projectedValue.value }
+        // nonmutating is to avoid crashes due to re-entrant when the get is called by a subscriber of the underlying subject
+        nonmutating
+        set { projectedValue.send(newValue) }
+    }
+
+    public let projectedValue: AlertTextSubject<NSAttributedString?, Never>
+
+    public init(wrappedValue: NSAttributedString?, placeholder: String? = nil) {
+        self.projectedValue = AlertTextSubject(placeholder: placeholder, initialValue: wrappedValue)
+    }
+}
+
+
+public final class AlertTextSubject<Output, Failure: Error>: Subject {
+
+    let placeholder: String?
+    private let subject: CurrentValueSubject<Output, Failure>
+
+    var value: Output { subject.value }
+
+    init(placeholder: String?, initialValue: Output) {
+        self.placeholder = placeholder
+        self.subject = CurrentValueSubject(initialValue)
+    }
+
+    public func send(_ value: Output) {
+        subject.send(value)
+    }
+
+    public func send(completion: Subscribers.Completion<Failure>) {
+        subject.send(completion: completion)
+    }
+
+    public func send(subscription: Subscription) {
+        subject.send(subscription: subscription)
+    }
+
+    public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+        subject.receive(subscriber: subscriber)
+    }
+}
