157523b0fdb9fbe54080ed706c6a98402a8770a3
Benedict Cohen <ben@benedictcohen.co.uk>
2023-06-25 20:11:30 +0100

    Adds UIStackView initializer

---
+++ Sources/SweetUI/Views/UIKit Extensions/UIStackView/UIStackView+Initializer.swift
@@ -0,0 +1,19 @@
+import Foundation
+import UIKit
+
+
+public extension UIStackView {
+
+    convenience init(axis: NSLayoutConstraint.Axis,  distribution: UIStackView.Distribution = .fill, alignment: UIStackView.Alignment = .fill, spacing: CGFloat = UIStackView.spacingUseDefault,  @ArrangedSubviewsBuilder arrangedSubviewsBuilder: () -> [UIView]) {
+        let arrangedSubviews = arrangedSubviewsBuilder()
+        for subview in arrangedSubviews {
+            subview.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.distribution = distribution
+        self.alignment = alignment
+        self.spacing = spacing
+    }
+}
