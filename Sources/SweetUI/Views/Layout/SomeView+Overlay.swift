fe74c7a2d58938a65b2fb8803d4cdf2ae82cc06a
Benedict Cohen <ben@benedictcohen.co.uk>
2023-07-08 14:31:37 +0100

    Adds ConstraintsBuilder to OverlayAlignment.init

--- Sources/SweetUI/Views/Layout/SomeView+Overlay.swift
+++ Sources/SweetUI/Views/Layout/SomeView+Overlay.swift
@@ -2,82 +2,7 @@ import Foundation
 import UIKit
 
 
-public struct OverlayAlignment {
-
-    public let constraintsFactory: (_ base: UIView, _ overlay: UIView) -> [NSLayoutConstraint]
-
-    public init(constraintsFactory: @escaping (_ base: UIView, _ overlay: UIView) -> [NSLayoutConstraint]) {
-        self.constraintsFactory = constraintsFactory
-    }
-}
-
-
-public extension OverlayAlignment {
-
-    static var topLeading: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.leadingAnchor.constraint(equalTo: overlay.leadingAnchor),
-            base.topAnchor.constraint(equalTo: overlay.topAnchor)]
-        }
-    }
-
-    static var top: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.centerXAnchor.constraint(equalTo: overlay.centerXAnchor),
-            base.topAnchor.constraint(equalTo: overlay.topAnchor)]
-        }
-    }
-
-    static var topTrailing: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.trailingAnchor.constraint(equalTo: overlay.trailingAnchor),
-            base.topAnchor.constraint(equalTo: overlay.topAnchor)]
-        }
-    }
-
-    static var leading: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.leadingAnchor.constraint(equalTo: overlay.leadingAnchor),
-            base.centerYAnchor.constraint(equalTo: overlay.centerYAnchor)]
-        }
-    }
-
-    static var center: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.centerXAnchor.constraint(equalTo: overlay.centerXAnchor),
-            base.centerYAnchor.constraint(equalTo: overlay.centerYAnchor)]
-        }
-    }
-
-    static var trailing: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.trailingAnchor.constraint(equalTo: overlay.trailingAnchor),
-            base.centerYAnchor.constraint(equalTo: overlay.centerYAnchor)]
-        }
-    }
-
-    static var bottomLeading: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.leadingAnchor.constraint(equalTo: overlay.leadingAnchor),
-            base.bottomAnchor.constraint(equalTo: overlay.bottomAnchor)]
-        }
-    }
-
-    static var bottom: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.centerXAnchor.constraint(equalTo: overlay.centerXAnchor),
-            base.bottomAnchor.constraint(equalTo: overlay.bottomAnchor)]
-        }
-    }
-
-    static var bottomTrailing: OverlayAlignment {
-        OverlayAlignment { base, overlay in
-            [base.trailingAnchor.constraint(equalTo: overlay.trailingAnchor),
-             base.bottomAnchor.constraint(equalTo: overlay.bottomAnchor)]
-        }
-    }
-}
-
+// MARK: - Core
 
 public extension SomeView {
 
@@ -102,5 +27,84 @@ public extension SomeView {
         NSLayoutConstraint.activate(overlayConstraints)
 
         return container
+    }
+}
+
+
+public struct OverlayAlignment {
+
+    public let constraintsFactory: (_ base: UIView, _ overlay: UIView) -> [NSLayoutConstraint]
+
+    public init(@ConstraintsBuilder constraintsFactory: @escaping (_ base: UIView, _ overlay: UIView) -> [NSLayoutConstraint]) {
+        self.constraintsFactory = constraintsFactory
+    }
+}
+
+
+// MARK: - In Bounds
+
+public extension OverlayAlignment {
+
+    static var topLeading: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.leadingAnchor.constraint(equalTo: overlay.leadingAnchor)
+            base.topAnchor.constraint(equalTo: overlay.topAnchor)
+        }
+    }
+
+    static var top: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.centerXAnchor.constraint(equalTo: overlay.centerXAnchor)
+            base.topAnchor.constraint(equalTo: overlay.topAnchor)
+        }
+    }
+
+    static var topTrailing: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.trailingAnchor.constraint(equalTo: overlay.trailingAnchor)
+            base.topAnchor.constraint(equalTo: overlay.topAnchor)
+        }
+    }
+
+    static var leading: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.leadingAnchor.constraint(equalTo: overlay.leadingAnchor)
+            base.centerYAnchor.constraint(equalTo: overlay.centerYAnchor)
+        }
+    }
+
+    static var center: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.centerXAnchor.constraint(equalTo: overlay.centerXAnchor)
+            base.centerYAnchor.constraint(equalTo: overlay.centerYAnchor)
+        }
+    }
+
+    static var trailing: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.trailingAnchor.constraint(equalTo: overlay.trailingAnchor)
+            base.centerYAnchor.constraint(equalTo: overlay.centerYAnchor)
+        }
+    }
+
+    static var bottomLeading: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.leadingAnchor.constraint(equalTo: overlay.leadingAnchor)
+            base.bottomAnchor.constraint(equalTo: overlay.bottomAnchor)
+        }
+    }
+
+    static var bottom: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.centerXAnchor.constraint(equalTo: overlay.centerXAnchor)
+            base.bottomAnchor.constraint(equalTo: overlay.bottomAnchor)
+        }
+    }
+
+    static var bottomTrailing: OverlayAlignment {
+        OverlayAlignment { base, overlay in
+            base.trailingAnchor.constraint(equalTo: overlay.trailingAnchor)
+            base.bottomAnchor.constraint(equalTo: overlay.bottomAnchor)
+        }
     }
 }
