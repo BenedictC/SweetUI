e41cc0229b15f2d30bedaaf6e86f79fc2f5a4015
Benedict Cohen <ben@benedictcohen.co.uk>
2023-07-08 14:30:26 +0100

    Adds inits to UIBarButtonItem so use of @objc can be avoided

---
+++ Sources/SweetUI/Views/UIKit Extensions/UIBarButtonItem/UIBarButtonItem+Initializer.swift
@@ -0,0 +1,48 @@
+import UIKit
+import Combine
+
+
+@available(iOS 14, *)
+public extension UIBarButtonItem {
+
+    convenience init(title: String? = nil, imageNamed imageName: String? = nil, action handler: @escaping () -> Void) {
+        let action = UIAction { _ in handler() }
+        let image = imageName.flatMap { UIImage(named: $0) }
+        self.init(title: title, image: image, primaryAction: action)
+    }
+
+    convenience init(title: String? = nil, systemImageNamed imageName: String, action handler: @escaping () -> Void) {
+        let action = UIAction { _ in handler() }
+        let image = UIImage(systemName: imageName)
+        self.init(title: title, image: image, primaryAction: action)
+    }
+
+    convenience init(systemItem: UIBarButtonItem.SystemItem, action handler: @escaping () -> Void) {
+        let action = UIAction { _ in handler() }
+        self.init(systemItem: systemItem, primaryAction: action, menu: nil)
+    }
+
+    convenience init(customView builder: () -> UIView) {
+        let customView = builder()
+        self.init(customView: customView)
+    }
+}
+
+
+@available(iOS 15, *)
+public extension UIBarButtonItem {
+
+    func selected<P: Publisher>(
+        _ publisher: P,
+        cancellableStorageHandler: CancellableStorageHandler = DefaultCancellableStorage.shared.store
+    ) -> Self where P.Output == Bool, P.Failure == Never {
+        // HACK ALERT! This causes a runtime crash due to a compiler bug related to the keyPath:
+        // assign(to: \.isHidden, from: publisher, cancellableStorageHandler: cancellableStorageHandler)
+        // So we have to do it the long way:
+        let cancellable = publisher.sink { [weak self] value in
+            self?.isSelected = value
+        }
+        cancellableStorageHandler(cancellable, self)
+        return self
+    }
+}
