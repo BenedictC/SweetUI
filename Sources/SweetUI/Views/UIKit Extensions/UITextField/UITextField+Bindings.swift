1699024abed2882bcde88fce1cd624d197bb94ad
Benedict Cohen <ben@benedictcohen.co.uk>
2023-10-05 11:41:28 +0100

    Adds UITextField bindings for non optional String subjects

--- Sources/SweetUI/Views/UIKit Extensions/UITextField/UITextField+Bindings.swift
+++ Sources/SweetUI/Views/UIKit Extensions/UITextField/UITextField+Bindings.swift
@@ -27,6 +27,26 @@ public extension SomeView where Self: UITextField {
         cancellableStorageHandler(cancellable, self)
         return self
     }
+
+    func text<S: Subject>(
+        _ subject: S,
+        options: UITextInputBindingOption = [],
+        cancellableStorageHandler: CancellableStorageHandler = DefaultCancellableStorage.shared.store
+    ) -> Self where S.Output == String, S.Failure == Never {
+        let cancellable = bindText(to: subject, options: options)
+        cancellableStorageHandler(cancellable, self)
+        return self
+    }
+
+    func attributedText<S: Subject>(
+        _ subject: S,
+        options: UITextInputBindingOption = [],
+        cancellableStorageHandler: CancellableStorageHandler = DefaultCancellableStorage.shared.store
+    ) -> Self where S.Output == NSAttributedString, S.Failure == Never {
+        let cancellable = bindAttributedText(to: subject, options: options)
+        cancellableStorageHandler(cancellable, self)
+        return self
+    }
 }
 
 
@@ -51,6 +71,24 @@ public extension UITextField {
         self.init()
         _ = self.attributedText(subject, options: options, cancellableStorageHandler: cancellableStorageHandler)
     }
+
+    convenience init<S: Subject>(
+        text subject: S,
+        options: UITextInputBindingOption = [],
+        cancellableStorageHandler: CancellableStorageHandler = DefaultCancellableStorage.shared.store
+    ) where S.Output == String, S.Failure == Never {
+        self.init()
+        _ = self.text(subject, options: options, cancellableStorageHandler: cancellableStorageHandler)
+    }
+
+    convenience init<S: Subject>(
+        text subject: S,
+        options: UITextInputBindingOption = [],
+        cancellableStorageHandler: CancellableStorageHandler = DefaultCancellableStorage.shared.store
+    ) where S.Output == NSAttributedString, S.Failure == Never {
+        self.init()
+        _ = self.attributedText(subject, options: options, cancellableStorageHandler: cancellableStorageHandler)
+    }
 }
 
 
@@ -65,6 +103,14 @@ public extension SomeView where Self: UITextField {
     func bindAttributedText<S: Subject>(to subject: S, options: UITextInputBindingOption = []) -> AnyCancellable where S.Output == NSAttributedString?, S.Failure == Never {
         makeBindings(for: subject, keyPath: \.attributedText, options: options)
     }
+
+    func bindText<S: Subject>(to subject: S, options: UITextInputBindingOption = []) -> AnyCancellable where S.Output == String, S.Failure == Never {
+        return makeBindings(for: subject, keyPath: \.text, defaultValue: "", options: options)
+    }
+
+    func bindAttributedText<S: Subject>(to subject: S, options: UITextInputBindingOption = []) -> AnyCancellable where S.Output == NSAttributedString, S.Failure == Never {
+        makeBindings(for: subject, keyPath: \.attributedText, defaultValue: NSAttributedString(string: ""), options: options)
+    }
 }
 
 
@@ -73,6 +119,28 @@ private extension SomeView where Self: UITextField {
     func makeBindings<V, S: Subject>(for subject: S, keyPath: ReferenceWritableKeyPath<Self, V>, options: UITextInputBindingOption) -> AnyCancellable where S.Output == V, S.Failure == Never {
         let send = self.addAction(for: .editingChanged) { textField, _ in
             subject.send(textField[keyPath: keyPath])
+        }
+        let editingDidEnd = self.addAction(for: [.editingDidEnd]) { textField, event in
+            // Changes from the subject are ignore while the textField has focus so
+            // resynchronize the value when editing ends.
+            _ = subject.sink { textField[keyPath: keyPath] = $0 }
+        }
+        let receive = subject.sink { [weak self] value in
+            guard let self = self else { return }
+            let shouldUpdate = !self.isFirstResponder || options.contains(.updatesTextWhenIsFirstResponder)
+            guard shouldUpdate else { return }
+            self[keyPath: keyPath] = value
+        }
+        return AnyCancellable {
+            editingDidEnd.cancel()
+            send.cancel()
+            receive.cancel()
+        }
+    }
+
+    func makeBindings<V, S: Subject>(for subject: S, keyPath: ReferenceWritableKeyPath<Self, V?>, defaultValue: V, options: UITextInputBindingOption) -> AnyCancellable where S.Output == V, S.Failure == Never {
+        let send = self.addAction(for: .editingChanged) { textField, _ in
+            subject.send(textField[keyPath: keyPath] ?? defaultValue)
         }
         let editingDidEnd = self.addAction(for: [.editingDidEnd]) { textField, event in
             // Changes from the subject are ignore while the textField has focus so
