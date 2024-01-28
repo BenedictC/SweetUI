e4f98e4c3ce69000e05074b1640c7ea8c2f0d61b
Benedict Cohen <ben@benedictcohen.co.uk>
2024-01-28 23:19:03 +0000

    Tidy

--- Sources/SweetUI/Views/Layout/SomeView+IfElse.swift
+++ Sources/SweetUI/Views/Layout/SomeView+IfElse.swift
@@ -5,16 +5,17 @@ import UIKit
 public extension SomeView where Self: UIView {
 
     // If the closures return differing types then the return value will be erased to UIView
-    func `if`<T: UIView>(_ value: Bool, _ ifHandler: (Self) -> T, `else` elseHandler: (Self) -> T) -> T {
-        value ? ifHandler(self) : elseHandler(self)
+    func `if`<T>(_ value: Bool, then ifTransform: (Self) -> T, `else` elseTransform: (Self) -> T) -> T {
+        value ? ifTransform(self) : elseTransform(self)
     }
 
-    func `if`(_ value: Bool, _ ifHandler: (Self) -> Self) -> Self {
-        value ? ifHandler(self) : self
+    func `if`(_ value: Bool, then transform: (Self) -> Self) -> Self {
+        value ? transform(self) : self
     }
 
-    // If the closure returns a different type then we have to erase to UIView
-    func `if`(_ value: Bool, _ ifHandler: (Self) -> UIView) -> UIView {
-        value ? ifHandler(self) : self
+    // If the closure returns a different type then we have to erase to UIView.
+    // If it is necessary to return a more specific type then add an else clause that explicitly states the type.
+    func `if`(_ value: Bool, then transform: (Self) -> UIView) -> UIView {
+        value ? transform(self) : self
     }
 }
