0efabcfddd7a80f6184552a66208b8dd43c45103
Benedict Cohen <ben@benedictcohen.co.uk>
2024-02-10 11:42:30 +0000

    Simplifies ifelse modifier

--- Sources/SweetUI/Views/Layout/SomeView+IfElse.swift
+++ Sources/SweetUI/Views/Layout/SomeView+IfElse.swift
@@ -4,18 +4,7 @@ import UIKit
 
 public extension SomeView where Self: UIView {
 
-    // If the closures return differing types then the return value will be erased to UIView
-    func `if`<T>(_ value: Bool, then ifTransform: (Self) -> T, `else` elseTransform: (Self) -> T) -> T {
-        value ? ifTransform(self) : elseTransform(self)
-    }
-
-    func `if`(_ value: Bool, then transform: (Self) -> Self) -> Self {
-        value ? transform(self) : self
-    }
-
-    // If the closure returns a different type then we have to erase to UIView.
-    // If it is necessary to return a more specific type then add an else clause that explicitly states the type.
-    func `if`(_ value: Bool, then transform: (Self) -> UIView) -> UIView {
-        value ? transform(self) : self
+    func `if`<T: SomeView>(_ condition: Bool, then ifTransform: (Self) -> T, else elseTransform: (Self) -> T = { (value: T) in value }) -> T {
+        condition ? ifTransform(self) : elseTransform(self)
     }
 }
