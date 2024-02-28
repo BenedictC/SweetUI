55348e1756af814ae53344f4ad5f0e641661c096
Benedict Cohen <ben@benedictcohen.co.uk>
2024-02-28 09:56:47 +0000

    Disables Macros due to compiler being too slow

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import CompilerPluginSupport
+// import CompilerPluginSupport
 
 
 let package = Package(
@@ -16,23 +16,24 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-        // .package(url: /* package url */, from: "1.0.0"),
+//        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .macro(
-            name: "MacrosPlugin",
-            dependencies: [
-                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-            ]
-        ),
+//        .macro(
+//            name: "MacrosPlugin",
+//            dependencies: [
+//                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+//                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+//            ]
+//        ),
 
         .target(
             name: "SweetUI",
-            dependencies: ["MacrosPlugin"]
+            dependencies: [
+//                "MacrosPlugin"
+            ]
         ),
 
         .testTarget(
