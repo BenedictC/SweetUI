0f3f3007b88936d8e29c830e1abfe208d525b9d8
Benedict Cohen <ben@benedictcohen.co.uk>
2024-03-04 13:54:12 +0000

    Disables macros

--- Sources/SweetUI/Macros/Macros.swift
+++ Sources/SweetUI/Macros/Macros.swift
@@ -1,16 +1,16 @@
 import Combine
 
-public enum ExposedBindingAccessLevel {
-    case `default`
-    case `private`
-    case `fileprivate`
-    case `internal`
-    case `public`
-}
-
-
-@attached(peer, names: arbitrary)
-public macro Exposed(name: String? = nil, accessLevel: ExposedBindingAccessLevel = .default, asOneWay: Bool = false) = #externalMacro(module: "MacrosPlugin", type: "ExposedBindingPeerMacro")
-
-@freestanding(expression)
-public macro Binding<T>(toPublishedAt keyPath: T) -> Binding<T> = #externalMacro(module: "MacrosPlugin", type: "BindingFactoryMacro")
+//public enum ExposedBindingAccessLevel {
+//    case `default`
+//    case `private`
+//    case `fileprivate`
+//    case `internal`
+//    case `public`
+//}
+//
+//
+//@attached(peer, names: arbitrary)
+//public macro Exposed(name: String? = nil, accessLevel: ExposedBindingAccessLevel = .default, asOneWay: Bool = false) = #externalMacro(module: "MacrosPlugin", type: "ExposedBindingPeerMacro")
+//
+//@freestanding(expression)
+//public macro Binding<T>(toPublishedAt keyPath: T) -> Binding<T> = #externalMacro(module: "MacrosPlugin", type: "BindingFactoryMacro")
