7d7bd247fdf2d7bbdc5a0430774de27c3ea51cec
Benedict Cohen <ben@benedictcohen.co.uk>
2024-08-27 23:44:10 +0100

    Adds stateChange to Control

--- Sources/SweetUI/Views/Content/Control.swift
+++ Sources/SweetUI/Views/Content/Control.swift
@@ -16,19 +16,40 @@ open class _Control: UIControl, TraitCollectionChangesProvider {
 
     private lazy var traitCollectionChangesController = TraitCollectionChangesController(initialTraitCollection: traitCollection)
     public var traitCollectionChanges: AnyPublisher<TraitCollectionChanges, Never> { traitCollectionChangesController.traitCollectionChanges }
+    private let stateSubject: CurrentValueSubject<UIControl.State, Never>
+    public let stateChanges: AnyPublisher<UIControl.State, Never>
 
+    override public var isEnabled: Bool {
+        get { super.isEnabled }
+        set { super.isEnabled = newValue; notifyOfStateChange() }
+    }
+    override public var isSelected: Bool {
+        get { super.isSelected }
+        set { super.isSelected = newValue; notifyOfStateChange() }
+    }
+    override public var isHighlighted: Bool {
+        get { super.isHighlighted }
+        set { super.isHighlighted = newValue; notifyOfStateChange() }
+    }
+    override public var isHidden: Bool {
+        get { super.isHidden }
+        set { super.isHidden = newValue; notifyOfStateChange() }
+    }
 
-    // TODO: Add a publisher that emits state changes
-    
 
     // MARK: Instance life cycle
 
     public init() {
+        let stateSubject = CurrentValueSubject<UIControl.State, Never>(.normal)
+        self.stateSubject = stateSubject
+        self.stateChanges = stateSubject.eraseToAnyPublisher()
         super.init(frame: .zero)
+        
         guard let bodyProvider = self as? _ViewBodyProvider else {
             preconditionFailure("_Control subclasses must conform to _ViewBodyProvider")
         }
         bodyProvider.initializeBodyHosting()
+
     }
 
     @available(*, unavailable)
@@ -42,5 +63,28 @@ open class _Control: UIControl, TraitCollectionChangesProvider {
     open override func traitCollectionDidChange(_ previous: UITraitCollection?) {
         super.traitCollectionDidChange(previous)
         traitCollectionChangesController.send(previous: previous, current: traitCollection)
+    }
+
+    public override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let result = super.beginTracking(touch, with: event)
+        notifyOfStateChange()
+        return result
+    }
+
+    public override func cancelTracking(with event: UIEvent?) {
+        super.cancelTracking(with: event)
+        notifyOfStateChange()
+    }
+
+    public override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        super.endTracking(touch, with: event)
+        notifyOfStateChange()
+    }
+
+
+    // MARK: State management
+
+    private func notifyOfStateChange() {
+        stateSubject.send(state)        
     }
 }
