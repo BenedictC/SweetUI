0dc39e6ac99ce47757990580c781ef63f3b3abd9
Benedict Cohen <ben@benedictcohen.co.uk>
2025-05-13 19:45:38 +0100

    Adds published extension to UIPageControl

---
+++ Sources/SweetUI/Views/UIKit Extensions/UIPageControl/UIPageControl+Publishers.swift
@@ -0,0 +1,28 @@
+import Foundation
+import UIKit
+
+
+public extension UIPageControl {
+    
+    func numberOfPages(_ publisher: some Publisher<Int, Never>) -> Self {
+        onChange(of: publisher) { $0.numberOfPages = $1 }
+    }
+    
+    func currentPage(_ publisher: some Publisher<Int, Never>) -> Self {
+        onChange(of: publisher) { $0.currentPage = $1 }
+        return self
+    }
+}
+
+
+public extension UIPageControl {
+
+    convenience init(numberOfPages: Int, currentPage subject: some Subject <Int, Never>) {
+        self.init(frame: .zero)
+        self.numberOfPages = numberOfPages
+        _ = onEvent(.valueChanged) { pageControl in
+            subject.send(pageControl.currentPage)
+        }
+        _ = onChange(of: subject) { $0.currentPage = $1 }
+    }
+}
