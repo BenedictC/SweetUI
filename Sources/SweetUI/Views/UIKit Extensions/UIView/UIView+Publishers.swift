a990259d1b130012694db215a65eb3719a7212e6
Benedict Cohen <ben@benedictcohen.co.uk>
2025-05-14 23:03:13 +0100

    Adds view modifiers for backgroundColor and userInteractionEnabled

--- Sources/SweetUI/Views/UIKit Extensions/UIView/UIView+Publishers.swift
+++ Sources/SweetUI/Views/UIKit Extensions/UIView/UIView+Publishers.swift
@@ -10,21 +10,27 @@ public extension UIView {
         // HACK ALERT! This causes a runtime crash due to a compiler bug related to the keyPath:
         // assign(to: \.isHidden, from: publisher, cancellableStorageProvider: cancellableStorageProvider)
         // So we have to do it the long way:
-        publisher.sink { [weak self] value in
-            self?.isHidden = value
-        }
-        .store(in: .current)
-        return self
+        onChange(of: publisher, perform: { $0.isHidden = $1 })
     }
 
     func alpha(_ publisher: some Publisher<CGFloat, Never>) -> Self {
         // HACK ALERT! This causes a runtime crash due to a compiler bug related to the keyPath:
         // assign(to: \.alpha, from: publisher, cancellableStorageProvider: cancellableStorageProvider)
         // So we have to do it the long way:
-        publisher.sink { [weak self] value in
-            self?.alpha = value
-        }
-        .store(in: .current)
-        return self
+        onChange(of: publisher, perform: { $0.alpha = $1 })
+    }
+
+    func backgroundColor(_ publisher: some Publisher<UIColor, Never>) -> Self {
+        // HACK ALERT! This causes a runtime crash due to a compiler bug related to the keyPath:
+        // assign(to: \.backgroundColor, from: publisher, cancellableStorageProvider: cancellableStorageProvider)
+        // So we have to do it the long way:
+        onChange(of: publisher, perform: { $0.backgroundColor = $1 })
+    }
+
+    func userInteractionEnabled(_ publisher: some Publisher<Bool, Never>) -> Self {
+        // HACK ALERT! This causes a runtime crash due to a compiler bug related to the keyPath:
+        // assign(to: \.isUserInteractionEnabled, from: publisher, cancellableStorageProvider: cancellableStorageProvider)
+        // So we have to do it the long way:
+        onChange(of: publisher, perform: { $0.isUserInteractionEnabled = $1 })
     }
 }
